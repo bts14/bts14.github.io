406be6e3fed47545a896bbdf3db851cad72ac904
ben servey <bts14@scasd.org>
2017-04-06 20:37:50 +0000

    add colors to GP

--- geometric patterns/main.js
+++ geometric patterns/main.js
@@ -1,12 +1,18 @@
 var NUM_CIRCLES = 12; 
 var circleDiameter;
 var circleRadius;
+var rVal;
+var bVal;
+var gVal;
 function setup(){
     createCanvas(480, 600);
     circleDiameter = width/NUM_CIRCLES;
     circleRadius=circleDiameter/2;
 }
 function draw(){
+  rVal=255;
+  gVal=0;
+  bVal=0;
   var isShifted = false;
   var y=height;
   while(y>=0){
@@ -17,12 +23,17 @@ function draw(){
       x=0;
     }
   while (x<= width){
+    fill(color(rVal,gVal,bVal));
+    stroke(color(rVal,gVal,bVal));
       ellipse(x,y,circleDiameter,circleDiameter);
       x=x+circleDiameter;
       
   }
     y=y-circleRadius;
     isShifted=!isShifted;
+    rVal=rVal-2;
+    gVal=gVal+7;
+    bVal=bVal+3;
 }
     
 }
